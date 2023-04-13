e6e286e808b366fd6dbded88bb18764bd0a3694d
shoham cohen <shohco@post.bgu.ac.il>
2023-04-13 20:44:57 +0300

    add the function to position interface and add method without implementation to guest.

--- src/main/java/org/example/BusinessLayer/Member.java
+++ src/main/java/org/example/BusinessLayer/Member.java
@@ -27,6 +27,66 @@ public class Member extends Guest implements Position{
     public Store getStore() {
         return store;
     }
+
+    @Override
+    public void changeStoreManagerPermissions(String storeManager, int storeID, StoreManager.permissionType newPermission) {
+
+    }
+
+    @Override
+    public void setPositionOfMemberToStoreManager(int storeID, String MemberToBecomeManager) {
+
+    }
+
+    @Override
+    public void setPositionOfMemberToStoreOwner(int storeID, String MemberToBecomeOwner) {
+
+    }
+
+    @Override
+    public void removeProductFromStore(int storeID, int productID) {
+
+    }
+
+    @Override
+    public void editProductName(int storeID, int productID, String newName) {
+
+    }
+
+    @Override
+    public void editProductPrice(int storeID, int productID, int newPrice) {
+
+    }
+
+    @Override
+    public void editProductCategory(int storeID, int productID, String newCategory) {
+
+    }
+
+    @Override
+    public void editProductDescription(int storeID, int productID, String newDescription) {
+
+    }
+
+    @Override
+    public void addProduct(int storeID, int productID, String productName, int itemsAmount, int price) {
+
+    }
+
+    @Override
+    public List<Purchase> getPurchaseHistory(int storeID) {
+        return null;
+    }
+
+    @Override
+    public int openStore(String name) {
+        return 0;
+    }
+
+    @Override
+    public void logout() {
+
+    }
     /*public List<Position> getPositions() {
         return this.positions;
     }*/
--- src/main/java/org/example/BusinessLayer/StoreManager.java
+++ src/main/java/org/example/BusinessLayer/StoreManager.java
@@ -1,11 +1,74 @@
 package org.example.BusinessLayer;
 
+import java.util.List;
+
 public class StoreManager  implements Position{
 
+    enum permissionType{}
     private Store store;
 
     @Override
     public Store getStore() {
         return store;
+    }
+
+    @Override
+    public void changeStoreManagerPermissions(String storeManager, int storeID, permissionType newPermission) {
+
+    }
+
+    @Override
+    public void setPositionOfMemberToStoreManager(int storeID, String MemberToBecomeManager) {
+
+    }
+
+    @Override
+    public void setPositionOfMemberToStoreOwner(int storeID, String MemberToBecomeOwner) {
+
+    }
+
+    @Override
+    public void removeProductFromStore(int storeID, int productID) {
+
+    }
+
+    @Override
+    public void editProductName(int storeID, int productID, String newName) {
+
+    }
+
+    @Override
+    public void editProductPrice(int storeID, int productID, int newPrice) {
+
+    }
+
+    @Override
+    public void editProductCategory(int storeID, int productID, String newCategory) {
+
+    }
+
+    @Override
+    public void editProductDescription(int storeID, int productID, String newDescription) {
+
+    }
+
+    @Override
+    public void addProduct(int storeID, int productID, String productName, int itemsAmount, int price) {
+
+    }
+
+    @Override
+    public List<Purchase> getPurchaseHistory(int storeID) {
+        return null;
+    }
+
+    @Override
+    public int openStore(String name) {
+        return 0;
+    }
+
+    @Override
+    public void logout() {
+
     }
 }
