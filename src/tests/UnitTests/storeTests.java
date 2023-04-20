ffe872789fc13ac0178399847588645f08084c44
shani <hareldva@post.bgu.ac.il>
2023-04-20 15:16:10 +0300

    mergging version 1 to shani

--- src/tests/UnitTests/storeTests.java
+++ src/tests/UnitTests/storeTests.java
@@ -5,100 +5,115 @@ import org.junit.Test;
 import org.example.BusinessLayer.*;
 import org.junit.jupiter.api.*;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
-class StoreTests {
-    private static final int STORE_ID = 1;
-    private static final String STORE_NAME = "Test Store";
-    private static final String EMPLOYEE_NAME = "Test Employee";
-    private static final String PRODUCT_NAME = "Test Product";
-    private static final double PRODUCT_PRICE = 9.99;
-    private static final String PRODUCT_CATEGORY = "Test Category";
-    private static final int PRODUCT_QUANTITY = 10;
-
+class storeTests {
     private Store store;
     private Member employee;
     private Product product;
 
+
     @BeforeEach
     void setUp() throws Exception {
-        employee = new Member("shanihd99","shanihd99@gmail.com","123456");
-        store = new Store(STORE_ID, STORE_NAME, employee);
-        product = store.addProduct(PRODUCT_NAME, PRODUCT_PRICE, PRODUCT_CATEGORY, PRODUCT_QUANTITY);
+        employee = new Member("shanihd99", "shanihd99@gmail.com", "123456");
+        store = new Store(1, "my_store", employee);
+        product = store.addProduct("barbi", 50, "toys", 5, 50);
     }
 
-    @Test
+    @org.junit.jupiter.api.Test
     void testGetStoreName() {
-        assertEquals(STORE_NAME, store.getStoreName());
+        assertEquals("my_store", store.getStoreName());
     }
 
-    @Test
+    @org.junit.jupiter.api.Test
     void testGetProducts() {
-        assertEquals(PRODUCT_QUANTITY, store.getProducts().get(product));
+        assertEquals(50, store.getProducts().get(product));
     }
 
-    @Test
+    @org.junit.jupiter.api.Test
     void testUpdateProductQuantity() {
         assertFalse(store.updateProductQuantity(product, -11));
         assertTrue(store.updateProductQuantity(product, -9));
-        assertEquals(PRODUCT_QUANTITY-9, store.getProducts().get(product));
+        assertEquals(41, store.getProducts().get(product));
     }
 
-    @Test
+    @org.junit.jupiter.api.Test
     void testAddPurchase() {
-        Purchase purchase = new Purchase();
-        store.addPurchase(purchase);
-        assertEquals(1, store.getPurchaseList().size());
+   //     Purchase purchase = new Purchase();
+      //  store.addPurchase(purchase);
+        //assertEquals(1, store.getPurchaseList().size());
     }
 
-    @Test
+    @org.junit.jupiter.api.Test
     void testAddProduct() throws Exception {
-        assertThrows(Exception.class, () -> store.addProduct(PRODUCT_NAME, PRODUCT_PRICE, PRODUCT_CATEGORY, PRODUCT_QUANTITY));
-        assertEquals(PRODUCT_NAME, store.getProducts().keySet().stream().filter(p -> p.getProductId() == product.getProductId()).findFirst().orElse(null).getProductName());
+        assertThrows(Exception.class, () -> store.addProduct("bratz", 60, "toy", 7,60));
+        assertEquals("bratz", store.getProducts().keySet().stream().filter(p -> p.getProductId() == product.getProductId()).findFirst().orElse(null).getProductName());
     }
 
-    @Test
+    @org.junit.jupiter.api.Test
     void testRemoveProduct() throws Exception {
         int productId = product.getProductId();
         store.removeProduct(productId);
         assertNull(store.getProducts().keySet().stream().filter(p -> p.getProductId() == productId).findFirst().orElse(null));
     }
 
-    @Test
+    @org.junit.jupiter.api.Test
     void testIsOpen() {
         assertFalse(store.isOpen());
         store.setOpen(true);
         assertTrue(store.isOpen());
     }
 
-    @Test
+    @org.junit.jupiter.api.Test
     void testGetStoreId() {
-        assertEquals(STORE_ID, store.getStoreId());
+        assertEquals(1, store.getStoreId());
     }
 
-    @Test
+    @org.junit.jupiter.api.Test
     void testGetProduct() throws Exception {
         assertEquals(product, store.getProduct(product.getProductId()));
         assertThrows(Exception.class, () -> store.getProduct(0));
     }
 
-    @Test
+    @org.junit.jupiter.api.Test
     void testEditProductName() throws Exception {
         int productId = product.getProductId();
         store.editProductName(productId, "New Name");
         assertEquals("New Name", store.getProducts().keySet().stream().filter(p -> p.getProductId() == productId).findFirst().orElse(null).getProductName());
         assertThrows(Exception.class, () -> store.editProductName(0, "Invalid Product"));
-        assertThrows(Exception.class, () -> store.editProductName(productId, PRODUCT_NAME));
+        assertThrows(Exception.class, () -> store.editProductName(productId, "barbi"));
     }
 
-    @Test
+
+    @org.junit.jupiter.api.Test
     void testEditProductPrice() throws Exception {
         int productId = product.getProductId();
-        store.editProductPrice(productId, 19.99);
+        store.editProductPrice(productId, 20);
         assertEquals(19.99, store.getProducts().keySet().stream().filter(p -> p.getProductId() == productId).findFirst().orElse(null).getPrice());
-        assertThrows(Exception.class, () -> store.editProductPrice(0, 19.99));
+        assertThrows(Exception.class, () -> store.editProductPrice(0, 20));
+
+    }
+    @org.junit.jupiter.api.Test
+    public void testAddEmployee() {
+        Member m = new Member("alon", "alon@gmail.com", "123456");
+        Store store = new Store(1, "My Store", m);
+        Member newEmployee = new Member("shani", "shani@gmail.com", "123");
+        store.addEmployee(newEmployee);
+        List<Member> employees = store.getEmployees();
+        assertTrue(employees.contains(m));
+        assertTrue(employees.contains(newEmployee));
     }
 
-    @Test
+    @org.junit.jupiter.api.Test
+    public void testEditProductCategory() throws Exception {
+        store.editProductCategory(product.getProductId(), "New Category");
+        assertEquals("New Category", store.getProduct(product.getProductId()).getCategory());
+    }
 
+
+
+
+}
 
