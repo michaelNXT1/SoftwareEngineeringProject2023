5359db7adef751cc9e974d94dbc8a7a3ef21599e
michaelNXT1 <tzahimi@post.bgu.ac.il>
2023-04-25 19:01:14 +0300

    Fix basic ShoppingCart tests

--- src/tests/UnitTests/ShoppingCartTest.java
+++ src/tests/UnitTests/ShoppingCartTest.java
@@ -9,23 +9,24 @@ import static org.junit.jupiter.api.Assertions.*;
 class ShoppingCartTest {
 
 
-    private ShoppingCart shoppingCart;
-    private Market market;
-    private Product product1;
-    private Product product2;
-    private Product product3;
-    private Store store1;
-    private Member member;
+    private static ShoppingCart shoppingCart;
+    private static Market market;
+    private static Product product1;
+    private static Product product2;
+    private static Product product3;
+    private static Store store1;
+    private static Member member;
 
     @BeforeAll
-    void beforeAll() throws Exception {
+    static void beforeAll() throws Exception {
         shoppingCart = new ShoppingCart();
-        product1 = new Product(1, "wine", 60.0, "alcohol","aa");
-        product2 = new Product(2, "cheese", 9.5, "milk","bb");
-        product3 = new Product(3, "steak", 120.0, "meat","cc");
         market = new Market();
+        market.signUpSystemManager("sysAdmin", "sys@admin.com", "YoImTheBaws");
         market.signUp("idan123", "idanlobel2@gmail.com", "wswsad32");
-        store1 = new Store(1, "Shufersal" , member);
+        store1 = new Store(1, "Shufersal", member);
+        product1 = store1.addProduct("wine", 60.0, "alcohol", 20, "aa");
+        product2 = store1.addProduct("cheese", 9.5, "milk", 4, "bb");
+        product3 = store1.addProduct("steak", 120.0, "meat", 10, "cc");
         //shoppingCart.addProduct(product2, 2, store1);
     }
 
@@ -37,23 +38,24 @@ class ShoppingCartTest {
 
     @Test
     void changeProductQuantity() throws Exception {
-        //shoppingCart.addProduct(product1, 2, store1);
-        shoppingCart.setProductQuantity(store1,0,12);
-        assertEquals(12 ,shoppingCart.shoppingBags.get(0).getProductList().get(product1));
+        shoppingCart.setProductQuantity(store1, 0, 12);
+        assertEquals(12, shoppingCart.shoppingBags.get(0).getProductList().get(product1.getProductId()));
     }
 
     @Test
     void removeProduct() throws Exception {
+        shoppingCart.setProductQuantity(store1, 0, 12);
         shoppingCart.removeProduct(store1, 0);
         //TODO: purchase doesn't hold Product, but PurchaseProduct, need to fix
-        assertFalse(shoppingCart.shoppingBags.get(0).getProductList().containsKey(product1));
+        assertFalse(shoppingCart.shoppingBags.get(0).getProductList().containsKey(product1.getProductId()));
     }
 
     @Test
     void purchaseShoppingCart() throws Exception {
+        shoppingCart.setProductQuantity(store1, product2.getProductId(), 3);
         Purchase purchase = shoppingCart.purchaseShoppingCart();
         //TODO: purchase doesn't hold Product, but PurchaseProduct, need to fix
-        assertTrue(purchase.getProductList().contains(product2));
+        assertTrue(purchase.getProductList().stream().anyMatch(pp -> pp.getProductId() == product2.getProductId()));
     }
 
     //טסט שאין מספיק מוצרים במלאי
