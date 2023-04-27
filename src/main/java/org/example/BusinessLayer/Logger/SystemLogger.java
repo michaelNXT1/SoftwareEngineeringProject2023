f1ff4172275536ff010ff05e8233131e2eeb6d3a
Alon Itzhaki <alonitz@post.bgu.ac.il>
2023-04-27 16:21:18 +0300

    fixing build and logs dep

--- src/main/java/org/example/BusinessLayer/Logger/SystemLogger.java
+++ src/main/java/org/example/BusinessLayer/Logger/SystemLogger.java
@@ -1,7 +1,11 @@
 package org.example.BusinessLayer.Logger;
 
-import org.apache.logging.log4j.LogManager;
+
+
 import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
+
+
 
 public class SystemLogger {
 
