8b3a9c14eabb735309afb2f75954c9974736af45
shani <hareldva@post.bgu.ac.il>
2023-05-10 14:00:11 +0300

    add open store page

---
+++ src/main/java/application/views/openStore/OpenStore.java
@@ -0,0 +1,39 @@
+package application.views.openStore;
+
+import CommunicationLayer.MarketController;
+import application.views.MainLayout;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.html.Header;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.router.Route;
+import org.springframework.beans.factory.annotation.Autowired;
+@Route(value = "OpenStore",layout = MainLayout.class)
+public class OpenStore  extends VerticalLayout {
+        private TextField storeNameField;
+        private Button submitButton;
+        private MarketController marketController;
+        private Header header;
+
+        @Autowired
+        public OpenStore(){
+            this.header = new Header();
+            this.header.setText("Open Store");
+            this.storeNameField = new TextField("store name");
+            this.submitButton = new Button("open", e -> openStore());
+            this.marketController = MarketController.getInstance();
+
+            add(header,storeNameField, submitButton);
+            setSizeFull();
+            setJustifyContentMode(JustifyContentMode.CENTER);
+            setDefaultHorizontalComponentAlignment(Alignment.CENTER);
+            getStyle().set("text-align", "center");
+        }
+
+        private void openStore() {
+            String storeName = storeNameField.getValue();
+
+            marketController.openStore( MainLayout.getSessionId(),storeName);
+        }
+
+}
