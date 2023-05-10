1d3020895498226fd598e5234dd3027eeaa3983f
shani <hareldva@post.bgu.ac.il>
2023-05-11 00:16:25 +0300

    add store manager permmisions

--- src/main/java/application/views/MainLayout.java
+++ src/main/java/application/views/MainLayout.java
@@ -162,7 +162,9 @@ public class MainLayout extends AppLayout {
         actionsMap.put("remove Product", () -> UI.getCurrent().navigate(RemoveProductFromStore.class));
         actionsMap.put("Open new Store", () -> UI.getCurrent().navigate(OpenStore.class));
         actionsMap.put("Edit Product", () -> UI.getCurrent().navigate(EditProduct.class));
-        actionsMap.put("add Store Manager Permissions", () -> UI.getCurrent().navigate(addStoreManagerPermissions.class));
+        actionsMap.put("add Store Manager Permission", () -> UI.getCurrent().navigate(addStoreManagerPermissions.class));
+        actionsMap.put("remove Store Manager Permission", () -> UI.getCurrent().navigate(removeStoreManagerPermissions.class));
+        actionsMap.put("close store", () -> UI.getCurrent().navigate(CloseStore.class));
 
 
 
