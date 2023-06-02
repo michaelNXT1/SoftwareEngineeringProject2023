a4afc2ffee639a75cc6ee3d5ef3260a7a519183f
shoham cohen <shohco@post.bgu.ac.il>
2023-06-02 16:13:23 +0300

    add repository for set of permission type

---
+++ src/main/java/DAOs/SetPermissionTypeDAO.java
@@ -0,0 +1,69 @@
+package DAOs;
+
+import BusinessLayer.StoreManager;
+import Repositories.ISetPermissionTypeRepository;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import java.util.Set;
+
+public class SetPermissionTypeDAO implements ISetPermissionTypeRepository {
+
+    private Set<StoreManager.permissionType> permissions;
+
+    public SetPermissionTypeDAO(Set<StoreManager.permissionType> permissions) {
+        this.permissions = permissions;
+    }
+
+    @Override
+    public void addPermission(StoreManager.permissionType permission) {
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        Transaction transaction = null;
+        try {
+            transaction = session.beginTransaction();
+            session.persist(permission);
+            transaction.commit();
+            permissions.add(permission);
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        } finally {
+            session.close();
+        }
+    }
+
+    @Override
+    public void removePermission(StoreManager.permissionType permission) {
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        Transaction transaction = null;
+        try {
+            transaction = session.beginTransaction();
+            if (permissions.contains(permission)) {
+                session.remove(permission);
+                permissions.remove(permission);
+            }
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        } finally {
+            session.close();
+        }
+
+    }
+
+    @Override
+    public Set<StoreManager.permissionType> getAllPermissions() {
+        return permissions;
+    }
+
+    @Override
+    public boolean contains(StoreManager.permissionType permission) {
+        return permissions.contains(permission);
+    }
+
+}
---
+++ src/main/java/Repositories/ISetPermissionTypeRepository.java
@@ -0,0 +1,13 @@
+package Repositories;
+
+import BusinessLayer.StoreManager;
+
+import java.util.Set;
+
+public interface ISetPermissionTypeRepository {
+    void addPermission(StoreManager.permissionType permission);
+    void removePermission(StoreManager.permissionType permission);
+    Set<StoreManager.permissionType> getAllPermissions();
+
+    boolean contains(StoreManager.permissionType permission);
+}
